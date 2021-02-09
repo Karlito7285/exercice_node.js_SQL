b4fb7a8a5c37551dca8a65a2de0b91b748fa4cea
Karlito7285 <73641416+Karlito7285@users.noreply.github.com>
2021-02-09 12:06:26 +0100

    configure mysql + creer une variable globale

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mysql = require('mysql');
+const util = require('util');
+
+// DotEnv
+require('dotenv').config()
+
+// MYSQL
+const db = mysql.createConnection({
+  host:  process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+})
+
+db.connect(
+  (err) => {
+    if (err) { throw err }
+    console.log('Connecté au serveur MySQL');
+  }
+)
+
+// DECLARE LA VARIABLE GLOBALE QUERY SQL
+global.querysql = util.promisify(db.query).bind(db)
+
 
 // EJS
 app.set('view engine', 'ejs');
